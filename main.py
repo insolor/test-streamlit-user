970226b6c767ab4cfbc0986d17c018329c9754ba
insolor <2442833+insolor@users.noreply.github.com>
2026-08-19 00:48:46 +0800

    Check if identity provider configured

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 import streamlit as st
 
+if not st.user:
+    st.error(
+        "Identity provider not configured. "
+        "See [User authentication](https://docs.streamlit.io/develop/concepts/connections/authentication)"
+    )
+    st.stop()
+
 if not st.user.is_logged_in:
     st.button("Log in with Google", on_click=st.login)
     st.stop()
