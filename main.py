9b546ff6c64bf5abcb358a78024fd1f5890cacb0
insolor <2442833+insolor@users.noreply.github.com>
2026-08-19 00:40:21 +0800

    Use example

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 import streamlit as st
 
-st.user
+if not st.user.is_logged_in:
+    st.button("Log in with Google", on_click=st.login)
+    st.stop()
+
+st.button("Log out", on_click=st.logout)
+st.markdown(f"Welcome! {st.user}")
